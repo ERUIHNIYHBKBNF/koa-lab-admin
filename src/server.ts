3a1e373ed540daf4bfbfba45682b87a0fc5915bc
ERUIHNIYHBKBNF <839203521@qq.com>
2022-05-04 18:25:50 +0800

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import Koa from 'koa';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
+import logger from './logger';
 import router from './router';
 
 // 初始化 Koa 应用实例
 const app = new Koa();
 
 // 注册中间件
+app.use(logger());
 app.use(cors());
 app.use(bodyParser());
 
