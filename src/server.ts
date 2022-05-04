7a0b14d64208c0429c5397f1e395902bd8bf31a4
ERUIHNIYHBKBNF <839203521@qq.com>
2022-05-04 20:45:04 +0800

    connect mysql

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import Koa from 'koa';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
+import { createConnection } from 'typeorm';
+import 'reflect-metadata';
+
 import logger from './logger';
 import router from './router';
 
-// 初始化 Koa 应用实例
-const app = new Koa();
+createConnection().then(() => {
+  // 初始化 Koa 应用实例
+  const app = new Koa();
 
-// 注册中间件
-app.use(logger()); // 打印请求日志
-app.use(cors());
-app.use(bodyParser());
+  // 注册中间件
+  app.use(logger()); // 打印请求日志
+  app.use(cors());
+  app.use(bodyParser());
 
-// 响应用户请求
-app.use(router.routes()).use(router.allowedMethods());
+  // 响应用户请求
+  app.use(router.routes()).use(router.allowedMethods());
 
-// 运行服务器
-app.listen(13875);
+  // 运行服务器
+  app.listen(13875);
+}).catch((err: string) => console.log('TypeORM connection error:', err));
